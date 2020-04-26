9793b07595998c252cb28618dc3853ae49d65533
dtrongphuc <dtrongphucdv@gmail.com>
2020-04-26 15:46:58 +0700

    finished banners

--- src/js/main.js
+++ src/js/main.js
@@ -5,16 +5,50 @@ $(document).ready(() => {
         var dropDownMenu = dropDown.querySelector(':scope > .dropdown-content');
         dropDown.addEventListener('mouseover' ,() => {
             dropDown.style.zIndex = 1060;
-            console.log('over');
             dropDownMenu.classList.replace('hide', 'show');
             $('.modal').modal('show');
         });
 
         dropDown.addEventListener('mouseout' ,() => {
             dropDown.style.zIndex = 1000;
-            console.log('hide');
-            $('.modal').modal('hide');
             dropDownMenu.classList.replace('show', 'hide');
+            $('.modal').modal('hide');
         });
-    })
+    });
+
+
+    $('#carousel-big').owlCarousel({
+        loop:true,
+        center:true,
+        margin:10,
+        autoplay:true,
+        autoplayTimeout:4000,
+        autoplayHoverPause:true,
+        responsive: {
+            0: {
+                items: 1
+            }
+        }
+    });
+
+    $('#carousel-small').owlCarousel({
+        loop:true,
+        center:false,
+        dots: false,
+        margin:10,
+        autoplay:true,
+        autoplayTimeout:3000,
+        autoplayHoverPause:true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 4
+            }
+        }
+    });
 });
